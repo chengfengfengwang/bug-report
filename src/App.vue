3f77f27323b2d980faff5529913e53bf6eb9b01a
wangchengfeng <wangchengfeng@xiaoyezi.com>
2022-07-06 18:48:19 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div id="app">
+    <sectionWrapper :title="" />
+  </div>
+</template>
+
+<script>
+import sectionWrapper from './sectionWrapper.vue';
+export default {
+  name: 'App',
+  components: { sectionWrapper }
+};
+</script>
